42c244f8907612c8569c8f81944ad4f9826e2666
sjh2428 <pok_gare@naver.com>
2019-10-02 19:48:32 +0900

    feat: add controller for /admin/users router
    회원 등록시키는 컨트롤러 구현

--- controllers/admin/user_controllers.js
+++ controllers/admin/user_controllers.js
@@ -1,8 +1,11 @@
 const sqlQuery = require("../../models/sql_query");
 
 module.exports = {
-    postController(req, res) { // url: /admin/users
-        
+    async postController(req, res) { // url: /admin/users
+        const { user_id, user_pass, user_name } = req.body;
+        await sqlQuery(`insert into tbl_user(user_id, user_pass, user_name)
+                                values('${user_id}', '${user_pass}', '${user_name}');`);
+        res.redirect("/admin/users");
     },
     getController(req, res) { // url: /admin/users
 
