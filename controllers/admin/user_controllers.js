7a0d0e10d26146491272aa2ea39d33345066cb5e
sjh2428 <pok_gare@naver.com>
2019-10-02 20:04:26 +0900

    docs: write user delete controller logic in idDelController
    idDelController에 주석으로 어떻게 동작시킬지 적어둠

--- controllers/admin/user_controllers.js
+++ controllers/admin/user_controllers.js
@@ -11,13 +11,15 @@ module.exports = {
         const getUsersData = await sqlQuery(`select user_id, user_pass, user_name, admin from tbl_user;`);
         res.render("/admin/users", {user: req.user, userData: getUsersData});
     },
-    idGetController(req, res) { // url: /admin/users/:id
+    async idGetController(req, res) { // url: /admin/users/:id
 
     },
-    idPutController(req, res) { // url: /admin/users/:id
+    async idPutController(req, res) { // url: /admin/users/:id
 
     },
-    idDelController(req, res) { // url: /admin/users/:id
-        
+    async idDelController(req, res) { // url: /admin/users/:id
+        // id에 해당하는 유저의 정보를 삭제해야 하지만
+        // 3개월 뒤에 삭제하도록 할 것임
+        // 테이블을 하나 더 두어서 탈퇴한 날짜와 아이디를 넣어두면 될듯
     }
 };
