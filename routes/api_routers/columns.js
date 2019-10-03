1353df4cc49df6e39255f502d01ef753bf975762
sjh2428 <pok_gare@naver.com>
2019-10-03 19:26:54 +0900

    add auth middleware to column routers

--- routes/api_routers/columns.js
+++ routes/api_routers/columns.js
@@ -2,16 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { postController, getController, idGetController,
     idPutController, idDelController } = require("../../controllers/api/column_controllers");
+const { doYouHaveRAuth, doYouHaveRWAuth } = require("../../modules/auth");
 
 // url: /api/columns
 
 router.route('/')
-    .post(postController)
-    .get(getController);
+    .post(doYouHaveRWAuth, postController)
+    .get(doYouHaveRAuth, getController);
 
 router.route('/:id')
-    .get(idGetController)
-    .put(idPutController)
-    .delete(idDelController);
+    .get(doYouHaveRAuth, idGetController)
+    .put(doYouHaveRWAuth, idPutController)
+    .delete(doYouHaveRWAuth, idDelController);
 
 module.exports = router;
