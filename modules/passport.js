cdd334f8e54bedd5c62c72776889bb9a57f49942
sjh2428 <pok_gare@naver.com>
2019-10-04 03:18:49 +0900

    feat: add github login router, passport, github login button
    라우터, 패스포트, github login버튼 추가 완료

--- modules/passport.js
+++ modules/passport.js
@@ -1,5 +1,7 @@
 const LocalStorage = require("passport-local").Strategy;
-const sql = require("../models/sql_query");
+const sqlQuery = require("../models/sql_query");
+const Strategy = require('passport-github2').Strategy;
+const { OAUTH_GITHUB_ID, OAUTH_GITHUB_SECRET, OAUTH_GITHUB_CALLBACK } = process.env;
 
 const passportSetting = (passport) => {
     passport.serializeUser((user, done) => {
@@ -9,13 +11,35 @@ const passportSetting = (passport) => {
     passport.deserializeUser(async(user, done) => {
         done(null, user);
     });
+
+    passport.use(new Strategy(
+        {
+            clientID: OAUTH_GITHUB_ID,
+            clientSecret: OAUTH_GITHUB_SECRET,
+            callbackURL: OAUTH_GITHUB_CALLBACK,
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            const id = profile.id;
+            const name = profile.username;
+            let userNo;
+            try {
+                const params = { "INSERT": [ id, name, "git" ], "SELECT": [ id ] };
+                await sqlQuery(`insert into tbl_user(user_id, user_name, user_pass) values(?, ?, ?)`, params.INSERT);
+                [ userNo ] = await sqlQuery(`select * from tbl_user where user_id=?`, params.SELECT);
+            } catch (error) {
+                return done(error);
+            }
+            return done(null, userNo);
+        })
+    );
     
     passport.use(new LocalStorage({
             usernameField: "user_id",
             passwordField: "user_pass"
         },
         async(username, password, done) => {
-            const sqlResult = await sql(`select * from tbl_user where user_id='${username}' and user_pass='${password}'`);
+            const params = [ username, password ];
+            const sqlResult = await sqlQuery(`select * from tbl_user where user_id=? and user_pass=?`, params);
             if (sqlResult.length) return done(null, sqlResult[0]);
             else return done(null, false, { message: "incorrect id or password" });
         }
--- routes/index_routers/index.js
+++ routes/index_routers/index.js
@@ -7,13 +7,16 @@ const { onlyPublic, onlyPrivate } = require('../../modules/auth');
 const { indexGetController } = require("../../controllers/index_controllers");
 
 // url: /
-
 router.get('/', onlyPublic, indexGetController);
 router.post('/', passport.authenticate("local", {
     successRedirect: "/todo",
     failureRedirect: "/",
     failureFlash: true
 }));
+router.get('/auth/github', passport.authenticate('github', { scope: ['user:login', 'user:name'] }));
+router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    return res.redirect('/');
+});
 router.use('/sign-up', onlyPublic, signupRouter);
 router.use('/todo', onlyPrivate, todoRouter);
 
