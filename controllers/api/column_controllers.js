eeb2853f0ebb81054be1358e82e32c7a4b3b80f0
sjh2428 <pok_gare@naver.com>
2019-10-03 22:37:39 +0900

    feat: add /api/columns POST controller
    column 추가시 맨 뒤에 들어가게끔 구현

--- controllers/api/column_controllers.js
+++ controllers/api/column_controllers.js
@@ -2,7 +2,13 @@ const sqlQuery = require("../../models/sql_query");
 
 module.exports = {
     async postController(req, res) { // url: /api/columns
-
+        const { body: { column_name, column_max_idx, project_id }, user: { user_id } } = req;
+        const params = [ column_name, column_max_idx + 1, project_id, user_id ];
+        const [ sqlRes ] = await sqlQuery(`insert into tbl_column(column_name, column_idx, project_id, created_by)
+            values(?, ?, ?, ?);`, params);
+        const statusCode = sqlRes.insertId === 0 ? 201 : 500;
+        res.status(statusCode);
+        res.end();
     },
     async getController(req, res) { // url: /api/columns
 
