15323697bf1bdb7eb27677543b31f7c782d70782
sjh2428 <pok_gare@naver.com>
2019-10-04 11:55:46 +0900

    fix: modulize init sql string
    초기 설정 쿼리문 query_str.js로 옮겨줌
    for..of문을 통해 순회하며 실행하도록 수정

--- models/query_str.js
+++ models/query_str.js
@@ -1,4 +1,74 @@
 module.exports = {
+    createUserTbl: `create table if not exists tbl_user(
+        user_id varchar(20) PRIMARY KEY NOT NULL,
+        user_pass varchar(20) NOT NULL,
+        user_name varchar(20) NOT NULL,
+        admin boolean DEFAULT FALSE
+    );`,
+    createProjectTbl: `create table if not exists tbl_project(
+        project_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        project_name varchar(40) NOT NULL,
+        project_idx int NOT NULL,
+        created_by varchar(20),
+        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
+    );`,
+    createColumnTbl: `create table if not exists tbl_column(
+        column_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        column_name varchar(40) NOT NULL,
+        column_idx int NOT NULL,
+        project_id int NOT NULL,
+        created_by varchar(20) NOT NULL,
+        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE,
+        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
+    );`,
+    createCardTbl: `create table if not exists tbl_card(
+        card_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        card_title varchar(100) NOT NULL,
+        card_contents TEXT NOT NULL,
+        card_idx int NOT NULL,
+        column_id int NOT NULL,
+        created_by varchar(20) NOT NULL,
+        FOREIGN KEY (column_id) REFERENCES tbl_column (column_id) ON UPDATE CASCADE,
+        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
+    );`,
+    createCardFilesTbl: `create table if not exists tbl_card_files(
+        card_id int NOT NULL,
+        card_file_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        card_file_type varchar(255) NOT NULL,
+        card_file_name varchar(255) NOT NULL,
+        FOREIGN KEY (card_id) REFERENCES tbl_card (card_id) ON UPDATE CASCADE ON DELETE CASCADE
+    );`,
+    createAuthTbl: `create table if not exists tbl_auth(
+        auth_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        project_id int NOT NULL,
+        user_id varchar(20) NOT NULL,
+        authority tinyint NOT NULL,
+        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE ON DELETE CASCADE,
+        FOREIGN KEY (user_id) REFERENCES tbl_user (user_id) ON UPDATE CASCADE ON DELETE CASCADE
+    );`,
+    createActionTbl: `create table if not exists tbl_action(
+        action_id int PRIMARY KEY NOT NULL,
+        action_name varchar(20) NOT NULL
+    );`,
+    createTargetTbl: `create table if not exists tbl_target(
+        target_id tinyint PRIMARY KEY NOT NULL,
+        target_name varchar(50) NOT NULL
+    );`,
+    createLogTbl: `create table if not exists tbl_log(
+        log_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+        project_id int NOT NULL,
+        created_by varchar(20),
+        created_time timestamp DEFAULT CURRENT_TIMESTAMP,
+        target tinyint NOT NULL,
+        target_id int NOT NULL,
+        action_id int NOT NULL,
+        log_describe varchar(255) NOT NULL,
+        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE,
+        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE,
+        FOREIGN KEY (action_id) REFERENCES tbl_action (action_id) ON UPDATE CASCADE,
+        FOREIGN KEY (target) REFERENCES tbl_target (target_id) ON UPDATE CASCADE
+    );`,
+
     insertUser: "insert into tbl_user(user_id, user_pass, user_name) values(?, ?, ?);",
     insertUserWithAdmin: "insert into tbl_user(user_id, user_pass, user_name, admin) values(?, ?, ?, ?);",
     findUserAll: "select user_id, user_pass, user_name, admin, exit_date from tbl_user;",
--- models/sql_init.js
+++ models/sql_init.js
@@ -1,75 +1,12 @@
 const sqlQuery = require("./sql_query");
+const { createUserTbl, createProjectTbl, createColumnTbl, createCardTbl,
+    createCardFilesTbl, createAuthTbl, createActionTbl, createTargetTbl, createLogTbl } = require("./query_str");
+
+const tblQuery = [ createUserTbl, createProjectTbl, createColumnTbl, createCardTbl,
+    createCardFilesTbl, createAuthTbl, createActionTbl, createTargetTbl, createLogTbl ];
 
 module.exports = async() => {
     console.log("database initializing");
-    await sqlQuery(`create table if not exists tbl_user(
-        user_id varchar(20) PRIMARY KEY NOT NULL,
-        user_pass varchar(20) NOT NULL,
-        user_name varchar(20) NOT NULL,
-        admin boolean DEFAULT FALSE
-    );`);
-    await sqlQuery(`create table if not exists tbl_project(
-        project_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        project_name varchar(40) NOT NULL,
-        project_idx int NOT NULL,
-        created_by varchar(20),
-        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
-    );`);
-    await sqlQuery(`create table if not exists tbl_column(
-        column_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        column_name varchar(40) NOT NULL,
-        column_idx int NOT NULL,
-        project_id int NOT NULL,
-        created_by varchar(20) NOT NULL,
-        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE,
-        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
-    );`);
-    await sqlQuery(`create table if not exists tbl_card(
-        card_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        card_title varchar(100) NOT NULL,
-        card_contents TEXT NOT NULL,
-        card_idx int NOT NULL,
-        column_id int NOT NULL,
-        created_by varchar(20) NOT NULL,
-        FOREIGN KEY (column_id) REFERENCES tbl_column (column_id) ON UPDATE CASCADE,
-        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE
-    );`);
-    await sqlQuery(`create table if not exists tbl_card_files(
-        card_id int NOT NULL,
-        card_file_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        card_file_type varchar(255) NOT NULL,
-        card_file_name varchar(255) NOT NULL,
-        FOREIGN KEY (card_id) REFERENCES tbl_card (card_id) ON UPDATE CASCADE ON DELETE CASCADE
-    );`);
-    await sqlQuery(`create table if not exists tbl_auth(
-        auth_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        project_id int NOT NULL,
-        user_id varchar(20) NOT NULL,
-        authority tinyint NOT NULL,
-        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE ON DELETE CASCADE,
-        FOREIGN KEY (user_id) REFERENCES tbl_user (user_id) ON UPDATE CASCADE ON DELETE CASCADE
-    );`);
-    await sqlQuery(`create table if not exists tbl_action(
-        action_id int PRIMARY KEY NOT NULL,
-        action_name varchar(20) NOT NULL
-    );`);
-    await sqlQuery(`create table if not exists tbl_target(
-        target_id tinyint PRIMARY KEY NOT NULL,
-        target_name varchar(50) NOT NULL
-    );`);
-    await sqlQuery(`create table if not exists tbl_log(
-        log_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        project_id int NOT NULL,
-        created_by varchar(20),
-        created_time timestamp DEFAULT CURRENT_TIMESTAMP,
-        target tinyint NOT NULL,
-        target_id int NOT NULL,
-        action_id int NOT NULL,
-        log_describe varchar(255) NOT NULL,
-        FOREIGN KEY (project_id) REFERENCES tbl_project (project_id) ON UPDATE CASCADE,
-        FOREIGN KEY (created_by) REFERENCES tbl_user (user_id) ON UPDATE CASCADE,
-        FOREIGN KEY (action_id) REFERENCES tbl_action (action_id) ON UPDATE CASCADE,
-        FOREIGN KEY (target) REFERENCES tbl_target (target_id) ON UPDATE CASCADE
-    );`);
+    for (const query of tblQuery) await sqlQuery(query);
     console.log("data init done");
 };
