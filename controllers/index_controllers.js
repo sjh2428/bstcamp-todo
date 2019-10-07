f01478ba725d68583138c92477057bc7abf51b95
sjh2428 <pok_gare@naver.com>
2019-10-07 17:06:51 +0900

    feat: add /sign-up POST controller
    회원가입 컨트롤러 구현
    회원가입 실패시 /sign-up페이지로 이동하며 flash msg 전달
    성공시 home(index)로 redirect

--- controllers/index_controllers.js
+++ controllers/index_controllers.js
@@ -1,4 +1,5 @@
 const sqlQuery = require("../models/sql_query");
+const { insertUser } = require("../models/query_str");
 
 module.exports = {
     indexGetController(req, res) { // url: /
@@ -15,8 +16,16 @@ module.exports = {
         const flashMsg = req.flash('fail_to_sign_up');
         res.render('index/sign_up', { message: flashMsg || undefined });
     },
-    signUpPostController(req, res) { // url: /sign-up
-
+    async signUpPostController(req, res) { // url: /sign-up
+        const { user_id, user_pass, user_name, admin } = req.body;
+        const params = [ user_id, user_pass, user_name, admin ];
+        try {
+            await sqlQuery(insertUser, params);
+            res.redirect('/');
+        } catch(err) {
+            req.flash("fail_to_sign_up", "회원가입에 실패하였습니다!");
+            res.redirect('/sign-up');
+        }
     },
     todoGetController(req, res) { // url: /todo
         res.render('todo/todo');
