af8b5330b857d7764010b5f1e3f6a2c1f34a593e
sjh2428 <pok_gare@naver.com>
2019-10-08 14:43:56 +0900

    feat: add draggable and index to column-wrapper, card-wrapper

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,11 @@
-console.log("this is asset javascript for front-end");
+const $ = (selector, base = document) => base.querySelector(selector);
+const $$ = (selector, base = document) => base.querySelectorAll(selector);
+
+$$('.column-wrapper').forEach(wrapper => {
+    wrapper.addEventListener('dragstart', (e) => {
+        e.target.style.opacity = 0.5;
+    });
+    wrapper.addEventListener('dragend', (e) => {
+        e.target.style.opacity = 1;
+    });
+})
