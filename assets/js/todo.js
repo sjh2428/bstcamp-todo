56887d4cdc8c6475cc992a73cda4d6f26f992f72
sjh2428 <pok_gare@naver.com>
2019-10-08 17:12:13 +0900

    feat: add function to get coordinates between columns
    컬럼 사이의 좌표를 얻기 위한 function 추가

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,6 +1,29 @@
 const $ = (selector, base = document) => base.querySelector(selector);
 const $$ = (selector, base = document) => base.querySelectorAll(selector);
 
+const getCoordinatesBetweenColumns = () => {
+    const res = [];
+    const columnWrappers = $$('.column-wrapper');
+    const mainDOM = document.querySelector('#main');
+    columnWrappers.forEach((wrapper, idx) => {
+        const { offsetLeft, offsetWidth } = wrapper;
+        switch (idx) {
+            case 0:
+                res.push({ left: 0, right: offsetLeft });
+                break;
+            case columnWrappers.length - 1:
+                res.push({ left: offsetLeft + offsetWidth, right: mainDOM.scrollWidth });
+                break;
+            default:
+                res.push({ left: offsetLeft - res[0].right, right: offsetLeft });
+        }
+    });
+    return res;
+}
+
+console.log(getCoordinatesBetweenColumns());
+
+
 $$('.column-wrapper').forEach(wrapper => {
     wrapper.addEventListener('dragstart', (e) => {
         e.target.style.opacity = 0.5;
@@ -8,4 +31,12 @@ $$('.column-wrapper').forEach(wrapper => {
     wrapper.addEventListener('dragend', (e) => {
         e.target.style.opacity = 1;
     });
-})
+});
+
+
+
+// document.querySelectorAll('.column-wrapper')[1].querySelector('.card-wrapper').addEventListener('dragover', (e) => {
+// 	e.preventDefault();
+// 	console.log(document.querySelector('#main').scrollLeft + window.event.clientX);
+	
+// })
